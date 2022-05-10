ab022e83b26aeca0bd355aeb5d51bfe5925cef37
Julien-Ama <inglesejulien@hotmail.fr>
2022-05-10 19:06:24 +0200

    Update api_geo.py

--- app/api_geo.py
+++ app/api_geo.py
@@ -1,7 +1,8 @@
 from configuration.config import GEO_API_URL, GEO_TOKEN
-from pprint import pprint
+
 
 import requests
+
 
 class GeoWrapper:
 
@@ -12,13 +13,14 @@ class GeoWrapper:
         self.get_coordinates()
 
     def get_coordinates(self):
-        result = requests.get(f"{GEO_API_URL}{self.input}.json?access_token={GEO_TOKEN}&language=fr")
-        if result.status_code == 200:
-            json_result = result.json() # conversion Json
-            print("geo")
-            pprint(json_result)
+        result = requests.get(f"{GEO_API_URL}{self.input}"
+                              f".json?access_token={GEO_TOKEN}&language=fr")
+        if result.status_code == 200 and self.input != "":
+            json_result = result.json()  # conversion Json
             if len(json_result['features']) > 0:
-                self.latitude = json_result['features'][0]['geometry']['coordinates'][0]
-                self.longitude = json_result['features'][0]['geometry']['coordinates'][1]
+                self.latitude =\
+                    json_result['features'][0]['geometry']['coordinates'][0]
+                self.longitude =\
+                    json_result['features'][0]['geometry']['coordinates'][1]
         else:
             print("Il y a un souci dans la requête")
